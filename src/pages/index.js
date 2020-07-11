35e34d0a1f1b12a0e7eee3ac373f8f9eea2159d3
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2020-07-11 11:55:00 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 export default () => (
   <Layout>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <h1>Hi! I'm building a Gatsby site as part of a tutorial!</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
       websites.
